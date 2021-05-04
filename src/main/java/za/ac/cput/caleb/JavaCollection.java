e9cb0e90be05f37c50d3aec7efa9ff533f861a60
Caleb Ruiters <35923446+CalebYPI@users.noreply.github.com>
2021-05-04 13:18:33 +0200

    Initial commit

---
+++ src/main/java/za/ac/cput/caleb/JavaCollection.java
@@ -0,0 +1,7 @@
+package za.ac.cput.caleb;
+
+import java.util.ArrayList;
+
+public class JavaCollection {
+    private ArrayList<String> thisThing;
+}
---
+++ src/test/java/za/ac/cput/caleb/JavaCollectionTest.java
@@ -0,0 +1,12 @@
+package za.ac.cput.caleb;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JavaCollectionTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
